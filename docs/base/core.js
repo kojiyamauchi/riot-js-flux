710751d043afa857d6bcd0bac01d825d95459442
kojiyamauchi <all.the.small.things@me.com>
2017-02-07 00:00:20 +0900

    core.js modified

--- docs/base/core.js
+++ docs/base/core.js
@@ -22,43 +22,6 @@ var jQuery = require('jQuery');
 
 jQuery(function ($) {
 
-    //eventToggleSyntax
-    (function ($) {
-        $.fn.eventToggle = function (fn) {
-            var args = arguments,
-                guid = fn.guid || jQuery.guid++,
-                i = 0,
-                toggler = function (event) {
-                    var lastToggle = (jQuery._data(this, 'lastToggle' + fn.guid) || 0) % i;
-                    jQuery._data(this, 'lastToggle' + fn.guid, lastToggle + 1);
-                    event.preventDefault();
-                    return args[lastToggle].apply(this, arguments) || false;
-                };
-            toggler.guid = guid;
-            while(i < args.length) {
-                args[i++].guid = guid;
-            }
-            return this.click(toggler);
-        };
-    })(jQuery);
-
-    //menuToggle
-    $('.btnMenu').eventToggle(function () {
-        $('nav.usersNavi').slideDown(500, 'swing');
-    }, function () {
-        $('nav.usersNavi').slideUp(500, 'swing');
-    });
-
-    //getCurrent
-    $('nav.usersNavi ul li a').each(function () {
-        var $href = $(this).attr('href');
-        if(location.href.match($href)) {
-            $(this).addClass('usersNaviCurrent');
-        } else {
-            $(this).removeClass('usersNaviCurrent');
-        }
-    });
-
     $('.displayed').addClass('on');
 
 });
