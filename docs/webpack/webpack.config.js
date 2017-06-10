129a28e6ffd8d7fc4d7c61fac10af574181d5022
kojiyamauchi <all.the.small.things@me.com>
2017-06-11 05:05:26 +0900

    change's browserify => webpack.

---
+++ docs/webpack/webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './base/core.js',
+  output: {
+    path: __dirname + 'js/',
+    filename: 'core.js'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.tag$/,
+      enforce: 'pre',
+      exclude: /node_modules/,
+      use: [
+      {
+        loader: 'riotjs-loader',
+        options: {
+          debug: true
+        }
+      }]
+    },
+    {
+      test: /\.js|\.tag$/,
+      enforce: 'post',
+      exclude: /node_modules/,
+      use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: 'es2015-riot'
+        }
+      }]
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.tag']
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      riot: 'riot'
+    })
+  ]
+};
